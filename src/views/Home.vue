9c1e85d400fb559a4a1dc3dff797bc7d91b45ce9
Fernando Bueno Dansi <fernandobuenodansi@gmail.com>
2021-08-16 20:03:35 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/cuidaapplogo2.png" />
     <br />
-    <h2 class="center">CuidaApp</h2>
     <h2 class="center">A sua saúde na palma da mão</h2>
     <p align="Justify">
       A automedicação, infelizmente, é um hábito muito frequente em nosso dia a
